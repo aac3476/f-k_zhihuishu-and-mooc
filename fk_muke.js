f63c06f5235a9684cc506e6fb32d99fb122a4a25
aac3476 <43756248+aac3476@users.noreply.github.com>
2018-11-26 19:50:49 +0800

    add muke
    慕课，打开评分页面，使用方法同某树

---
+++ fk_muke.js
@@ -0,0 +1,12 @@
+document.body.appendChild(document.createElement('script')).src='//code.jquery.com/jquery-1.9.1.min.js';
+setTimeout(function(){
+    $(".s label:last-child").each(
+        function(){
+            $(this).click();
+        });
+    $(".j-textarea").each(
+        function(){
+            $(this).val("全五分好评"); //这里是点评的内容
+        });
+},2000)
+
